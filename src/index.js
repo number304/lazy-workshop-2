a271e56f683bb75d8e28cf2b7337db6c6e9ce4e6
number304 <jmpmateo3042013@gmail.com>
2021-03-25 20:25:43 -0500

    random image generator done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+const min = 1, max = 122
+const random = () => Math.floor(Math.random() * (max - min)) + min
 
-console.log('Happy hacking :)')
+const createImageNode = () => {
+    const container = document.createElement('div')
+    container.className= 'p-4'
+
+    const imagen = document.createElement('img')
+    imagen.className = 'mx-auto'
+    imagen.width = '320'
+    imagen.src = `https://randomfox.ca/images/${random()}.jpg`
+
+    container.appendChild(imagen)
+
+    return container
+}
+
+const mountNode = document.getElementById('images')
+
+const addButton = document.querySelector('button')
+
+const addImage = () => {
+    const newImage = createImageNode()
+    mountNode.append(newImage)
+}
+
+addButton.addEventListener('click', addImage)
